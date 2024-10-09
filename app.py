5623722ec76520fd0b711c94384c0f649adafdf0
skhir <skhirtladze.tornike@gmail.com>
2024-10-09 11:52:33 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,392 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import datetime
+from scraping_coara import fetch_signatories_data, save_to_csv
+
+
+# Load the data
+coara_df = pd.read_csv("coara_signatories.csv")
+
+# Load world map from a local shapefile
+# Make sure to replace 'path_to_your_shapefile/ne_110m_admin_0_countries.shp' with your actual file path
+world = gpd.read_file("../CoARA_Signatories/earth/ne_110m_admin_0_countries.shp")
+
+# Get a list of valid country names + 'Timor-Leste', because in the dataset 'East Timor' is mentioned as 'Timor-Leste'
+valid_countries = world['ADMIN'].tolist()
+
+coara_df['Country'] = coara_df['Country'].replace('Timor-Leste', 'East Timor')
+
+# Filter the DataFrame to keep only valid countries
+df_filtered = coara_df[coara_df['Country'].isin(valid_countries)]
+
+# Group by country and count organizations
+country_counts = df_filtered.groupby(['Country']).size().reset_index(name='Counts')
+
+# Merge with world map
+merged = world.set_index('ADMIN').join(country_counts.set_index('Country'))
+
+st.set_page_config(page_title="CoARA Signatories")
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Initialize session state for both sidebars if they don't exist
+if 'selected_nav' not in st.session_state:
+    st.session_state['selected_nav'] = "📖 About"  # Default to "📖 About"
+if 'coara_data' not in st.session_state:
+    st.session_state['coara_data'] = "Clear Selection"  # Default to "Clear Selection"
+# ------------------------
+if 'last_updated' not in st.session_state:
+    st.session_state['last_updated'] = f"**2024-10-09**"
+
+
+# Function to clear the selected navigation when coara_data is chosen
+def clear_nav_if_coara_selected():
+    if st.session_state['coara_data'] != "Clear Selection":
+        st.session_state['selected_nav'] = "Clear Selection"
+
+
+# First Sidebar (Main Navigation)
+st.sidebar.header("Interactive Dashboard")
+selected_nav = st.sidebar.selectbox(
+    "Navigate",
+    ["Clear Selection"] + ["📖 About", "📊 Insights"],
+    key='selected_nav',
+    label_visibility="hidden",
+    on_change=lambda: st.session_state.update({'coara_data': 'Clear Selection'}),
+)
+
+# Second Sidebar (CoARA Data)
+st.sidebar.header("Data Source")
+coara_data = st.sidebar.selectbox(
+    "Navigate",
+    ["Clear Selection"] + ["📝 About The Data", "🌐 Update"],
+    key='coara_data',
+    label_visibility="hidden",
+    on_change=clear_nav_if_coara_selected,
+)
+
+if not selected_nav and not coara_data:
+    st.title(f"About the Authors")
+
+
+# Display content based on the selected option
+
+if selected_nav == "📖 About" and coara_data == "Clear Selection":
+
+
+    st.markdown("""
+        ### About the Dashboard
+        This user-friendly dashboard offers up-to-date information on organizations that have signed the
+        **Agreement on Reforming Research Assessment (ARRA)**, allowing users to easily explore
+        signatories by country.
+        
+        Voluntarily developed by independent researchers, the data visualization tool sources
+        information directly from the official website of the Coalition for **Advancing Research
+        Assessment (CoARA)** and is automatically updated.
+        
+        For more information on the responsible research assessment movement, visit the official
+        website: https://coara.eu/        
+        """)
+
+elif selected_nav == "📊 Insights" and coara_data == "Clear Selection":
+    # Streamlit App
+    st.title('Signatories Overview')
+
+    # Distribution of Universities by Country
+    st.subheader('Insights')
+
+    st.write(f"Last updated: **{st.session_state['last_updated']}**")
+
+    country_count = df_filtered['Country'].value_counts()
+    st.write("This section features a bar chart that shows the number of CoARA signatories for each country. You can select countries to display in the bar chart for comparison.")
+    select = st.multiselect(label="Select the Countries to Include in the Bar Chart for Comparison", options=df_filtered['Country'].unique(), placeholder="Select the countries")
+    if select:
+        filtered_country_count = country_count[country_count.index.isin(select)]
+        country_fig = px.bar(
+            filtered_country_count,
+            x=filtered_country_count.index,
+            y=filtered_country_count.values,
+            labels={'index': 'Country', 'y': 'Number of Signatories'},
+            title='Number of Signatories by Selected Countries',
+            text=filtered_country_count.values
+        )
+        country_fig.update_traces(texttemplate='%{text}', textposition='outside')
+        country_fig.update_layout(yaxis=dict(tickvals=[]))
+        country_fig.update_layout(xaxis=dict(tickangle=270))
+        country_fig.update_layout(xaxis_title="Countries")
+
+        country_fig.update_layout(
+            title={
+                'text': 'Number of Signatories by Country',
+                'font': {'size': 22}  # Set title font size
+            },
+            xaxis_title={
+                'text': "Countries",
+                'font': {'size': 16}
+            },
+            yaxis_title={
+                'text': "Number of Signatories",
+                'font': {'size': 16}
+            }
+        )
+
+        st.plotly_chart(country_fig)
+    else:
+        country_fig = px.bar(
+            country_count,
+            x=country_count.index,
+            y=country_count.values,
+            labels={'index': 'Country', 'y': 'Number of Signatories'},
+            title='Number of Signatories by Country',
+            text=country_count.values
+        )
+        country_fig.update_traces(texttemplate='%{text}', textposition='outside')
+        country_fig.update_layout(yaxis=dict(tickvals=[]))
+        country_fig.update_layout(xaxis=dict(tickangle=270))
+        country_fig.update_layout(xaxis_title="Countries")
+
+        # Increase font size for title and axis
+        country_fig.update_layout(
+            title={
+                'text': 'Number of Signatories by Country',
+                'font': {'size': 22}  # Set title font size
+            },
+            xaxis_title= {
+                'text': "Countries",
+                'font': {'size': 16}
+            },
+            yaxis_title= {
+                'text': "Number of Signatories",
+                'font': {'size': 16}
+            },
+            font=dict(size=12),  # Set axis font size
+            xaxis=dict(tickangle=270),
+            yaxis=dict(tickvals=[])
+        )
+        # Add hover text instruction for the users
+        country_fig.update_layout(
+            hovermode="x unified",
+            annotations=[
+                dict(
+                    text="Switch to fullscreen or click and drag to explore different areas of the bar chart",
+                    xref="paper", yref="paper",
+                    x=0.5, y=1.5, showarrow=False, font=dict(size=12), xanchor="center"
+                )
+            ]
+        )
+        st.plotly_chart(country_fig)
+
+    st.write("")
+    st.write("")
+
+    # Show raw data
+    st.subheader('List of the Signatories per Country')
+
+    selected_country = st.selectbox(label="Select a country to view the organizations that have signed the ARRA", options=df_filtered['Country'].unique(), index=None, placeholder="Select a country")
+    if selected_country:
+        selected_country_dataframe = df_filtered[df_filtered['Country']==selected_country]
+        if len(selected_country_dataframe) == 1:
+            st.write(f"There is only one signatory in {selected_country}")
+            st.write("### Signatory:")
+            for signatory in selected_country_dataframe['Organization'].values:
+                (f"- **{signatory}**")
+        else:
+            st.write(f"There are {len(selected_country_dataframe)} signatories in {selected_country}")
+            st.write("### Signatories:")
+            for signatory in selected_country_dataframe['Organization'].values:
+                (f"- **{signatory}**")
+
+
+    st.write("")
+    st.write("")
+
+
+    # Map Plot
+    st.subheader('Map of Signatories')
+
+    st.write(f"This map shows the distribution of signatories by country")
+
+    select_map_style = st.selectbox(label="Select the style in which the map is displayed", options=["Static Map", "Interactive Map"], index=0)
+
+    if select_map_style == "Static Map":
+        # Plotting
+        fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+        merged.boundary.plot(ax=ax, linewidth=1)
+        merged.plot(column='Counts', ax=ax, legend=True, cmap='YlOrBr',
+                    legend_kwds={'label': "Number of Signatories by Country",
+                                 'orientation': "horizontal"},
+                    missing_kwds={"color": "lightgrey", "label": "No Data"})
+
+
+        plt.title('Distribution of Signatories by Country', fontsize=22)
+
+        # Add instruction as an annotation
+        plt.annotate(
+            'To have a closer look, select the interactive map option and zoom in',
+            xy=(0.5, -0.1), xycoords='axes fraction',
+            ha='center', fontsize=10, color='gray'
+        )
+        # plt.xlabel('Longitude')
+        # plt.ylabel('Latitude')
+        plt.xticks([])
+        plt.yticks([])
+        st.pyplot(fig)
+
+    else:
+        # Create choropleth map for signatories
+        choropleth = px.choropleth(
+            merged.reset_index(),  # Reset index to access columns easily
+            locations='ADMIN',  # Column with country names
+            locationmode='country names',  # Match on country names
+            color='Counts',  # Column with values to color countries by (signatories count)
+            color_continuous_scale='YlOrBr',  # Color scale matching the original Matplotlib
+            range_color=(0, merged['Counts'].max()),  # Range for color scale based on signatories
+            labels={'ADMIN': 'Country', 'Counts': 'Number of Signatories'},  # Label for the legend
+            title='Distribution of Signatories by Country' # Title for the map
+
+        )
+
+        # Update the layout for appearance and readability
+        choropleth.update_layout(
+            template='plotly_dark',
+            plot_bgcolor='rgba(0, 0, 0, 0)',  # Transparent background for the plot
+            paper_bgcolor='rgba(0, 0, 0, 0)',  # Transparent background for the paper
+            geo_bgcolor='white',
+            margin=dict(l=0, r=0, t=50, b=0),  # Adjust margins
+            height=500,  # Set the height of the map
+            geo=dict(
+                showframe=False,  # Hide frame around the map
+                showcoastlines=True,  # Show coastlines for better distinction
+                projection_type='natural earth'  # Use 'natural earth' projection
+            )
+        )
+
+        # Display the map in Streamlit
+        st.plotly_chart(choropleth)
+
+
+    st.write("")
+    st.write("")
+
+
+    st.subheader("Post-Soviet Countries")
+
+    post_soviet_countries = ["Armenia", "Azerbaijan", "Belarus", "Estonia", "Georgia", "Kazakhstan", "Kyrgyzstan",
+                             "Latvia", "Lithuania", "Moldova", "Russia", "Tajikistan", "Turkmenistan", "Ukraine",
+                             "Uzbekistan"]
+    # Filter the DataFrame to keep only valid countries
+    post_soviet_df = coara_df[coara_df['Country'].isin(post_soviet_countries)]
+
+    # Group by country and count organizations
+    post_soviet_countries_count = post_soviet_df['Country'].value_counts()
+    post_soviet_countries_fig = px.bar(
+        post_soviet_df,
+        x=post_soviet_countries_count.index,
+        y=post_soviet_countries_count.values,
+        labels={'index': 'Country', 'y': 'Number of Signatories'},
+        title='Number of Signatories From Post-Soviet Countries',
+        text=post_soviet_countries_count.values
+    )
+    post_soviet_countries_fig.update_traces(texttemplate='%{text}', textposition='outside')
+    post_soviet_countries_fig.update_layout(yaxis=dict(tickvals=[]))
+    post_soviet_countries_fig.update_layout(xaxis=dict(tickangle=270))
+    post_soviet_countries_fig.update_layout(xaxis_title="Countries")
+
+    post_soviet_countries_fig.update_layout(
+        title={
+            'text': 'Number of Signatories From Post-Soviet Countries',
+            'font': {'size': 22}  # Set title font size
+        },
+        xaxis_title={
+            'text': "Countries",
+            'font': {'size': 16}
+        },
+        yaxis_title={
+            'text': "Number of Signatories",
+            'font': {'size': 16}
+        },
+        font=dict(size=12),  # Set axis font size
+        xaxis=dict(tickangle=270),
+        yaxis=dict(tickvals=[])
+    )
+
+    st.plotly_chart(post_soviet_countries_fig)
+
+
+
+elif coara_data == "📝 About The Data":
+    st.markdown('''
+        ### The Data Source
+        The dataset used in this app was scraped from the [CoARA Signatories page](https://coara.eu/signatories). It includes a list of organizations from various countries that have signed the CoARA agreement. The data consists of:
+        - **Organization Name**: The official name of the signatory organization.
+        - **Country**: The country where the organization is based.
+
+        
+    ''')
+
+    csv = df_filtered.to_csv(index=False)
+    # Create two columns
+    col1, col2 = st.columns([2, 1])  # Adjust column widths if necessary
+
+    # Add description on the left column
+    with col1:
+        st.write(
+            "Click the button to download the dataset containing the number of signatories per country as a CSV file.")
+
+    # Add download button on the right column
+    with col2:
+        st.download_button(
+            label="Download CSV",
+            data=csv,
+            file_name='CoARA_Signatories.csv',
+            mime='text/csv',
+        )
+
+
+elif coara_data == "🌐 Update":
+    st.title("Update the Data using Web Scraping")
+    last_updated = st.empty()
+    st.write("Web scraping takes a lot of time to gather up-to-date data.")
+    st.write("After clicking the button, stay on the current page to ensure the scraping process isn't interrupted.")
+    scraping_status = st.write("After the process is completed, the most recent update date will reflect the current date.")
+
+    if st.button("Scrape Data"):
+        start_scraping = st.empty()
+        start_scraping.write("You started scraping data...")
+
+        # Fetch data from the CoARA website
+        df = fetch_signatories_data()
+
+        # Save the DataFrame to a CSV file (optional)
+        save_to_csv(df)
+
+        st.session_state['last_updated'] = f"{datetime.date.today()}"
+        last_updated.write(f"Last Updated: **{datetime.date.today()}**")
+        start_scraping.empty()
+
+else:
+    st.title("The Application")
+
+    st.markdown("""
+        This project is a **data visualization app** built using **Streamlit** that scrapes and presents data about organizations signing the CoARA (Coalition for Advancing Research Assessment) agreement. 
+        It allows users to explore the list of signatories by country, visualize the data using maps and charts, and update the dataset in real-time via web scraping.
+
+        ### Key Features:
+        - **Data Scraping**: The app scrapes data from the CoARA website, extracting information about organizations and their associated countries.
+        - **Data Visualization**: It uses **Plotly** and **Matplotlib** for visualizing the distribution of signatories, including a **choropleth map** to display the global spread of organizations.
+        - **Interactive Insights**: Users can explore and filter the number of signatories by country, compare statistics, and view the list of organizations per country.
+        - **Real-time Data Update**: Users can scrape the latest data from the CoARA website and save it as a CSV file for further use.
+
+        ### Tools and Libraries Used:
+        - **Streamlit** for building the user interface.
+        - **BeautifulSoup** and **requests** for web scraping to gather data from the CoARA website.
+        - **Pandas** for data processing.
+        - **Geopandas** for geospatial analysis.
+        - **Plotly** and **Matplotlib** for data visualization.
+        
+        """)
+
---
+++ scraping_coara.py
@@ -0,0 +1,127 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin  # To handle relative URLs
+
+
+def fetch_signatories_data(url="https://coara.eu/agreement/signatories/"):
+    """
+    Function to scrape signatories' data from the COARA website.
+
+    Args:
+    url (str): URL of the COARA signatories page.
+
+    Returns:
+    DataFrame: Pandas DataFrame containing scraped country and organization data.
+    """
+
+    try:
+
+        # Send a request to fetch the webpage
+        response = requests.get(url)
+
+        # Parse the webpage content
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        countries = ['All']  # During the first iteration, there is no span.text for the span element.
+        countries_href = []  # During the first iteration, there is no href for the href element.
+
+        # Use CSS selector to target the divs
+        for row in soup.select('#signatories > div > div.flex.items-center.flex-wrap.justify-center.mb-12'):
+            links = row.find_all('a')
+
+            # Iterate through the links and extract href and text from <span>
+            for link in links:
+                href = link.get('href')
+                if href:
+                    countries_href.append(href)
+
+                span = link.find('span', class_='py-[14px]')
+                if span:
+                    countries.append(span.text)
+
+        # Remove first 'All' entries
+        countries.pop(0)
+        countries_href.pop(0)
+
+        # Global list to collect all h3 text items
+        h3_list = []
+        data = dict()
+
+        def scrape_page(any_url):
+            mylist = []
+            # Send a request to fetch the webpage
+            response = requests.get(any_url)
+
+            # Parse the webpage content
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Extract h3 elements from the page
+            for row in soup.find_all('div', class_='grid gird-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-20'):
+                all_h3 = row.find_all('h3', class_="mt-4 mb-20 text-left txt-lg")
+                for h3 in all_h3:
+                    h3_text = h3.get_text(strip=True)
+                    mylist.append(h3_text)
+                    h3_list.append(h3_text)
+
+            return mylist, soup
+
+        for country_index, country in enumerate(countries):
+            # Initialize list to store h3 items per country
+            data[country] = []
+
+            # Start scraping the first page
+            current_url = countries_href[country_index]
+
+            while current_url:
+                # Scrape the current page
+                mylist, soup = scrape_page(current_url)
+
+                # Accumulate the h3 items for this country
+                data[country].extend(mylist)
+
+                # Handle pagination (look for the next page link)
+                next_page_div = soup.find('div', class_='-mt-px w-0 flex-1 flex justify-end')
+
+                if next_page_div:
+                    # Find the first <a> tag, which is assumed to be the "next" button
+                    next_link = next_page_div.find('a')
+
+                    if next_link:
+                        # Get the href and join it with the base URL if it's relative
+                        next_url = next_link.get('href')
+                        current_url = urljoin(current_url, next_url)
+                    else:
+                        break
+                else:
+                    break
+
+        # Flatten the dictionary
+        rows = []
+        for country, organizations in data.items():
+            for org in organizations:
+                rows.append({'Country': country.strip(), 'Organization': org.strip()})
+
+        # Create a dataframe from the flattened data
+        my_df = pd.DataFrame(rows, columns=['Country', 'Organization'])
+
+        return my_df
+    except:
+        pass
+
+def save_to_csv(df, filename="coara_signatories.csv"):
+    """
+    Save the DataFrame to a CSV file.
+
+    Args:
+    df (DataFrame): The DataFrame to save.
+    filename (str): The name of the file to save the data.
+    """
+    try:
+        df.to_csv(filename, index=False)
+        # print(f"Data saved to {filename}")
+    except:
+        pass
+
+
+
